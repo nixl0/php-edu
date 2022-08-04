17c0502f53bd6ee37a83a1bdc9e89372c658ba50
Anton <mferra@usa.com>
2022-08-04 06:46:05 +0300

    delete style from Debug.php, add debug.css classes instead

--- src/debug/Debug.php
+++ src/debug/Debug.php
@@ -6,14 +6,14 @@ class Debug
 {
     public static function prn($content)
     {
-        echo '<pre style="background: palegreen; font-family: monospace; font-weight: bold; border: 1px solid black; padding: 2px">';
+        echo '<pre class="debug-prn">';
         print_r($content);
         echo '</pre>';
     }
 
     public static function dd($content)
     {
-        echo '<pre style="background: palegreen; font-family: monospace; font-weight: bold; border: 1px solid black; padding: 2px">';
+        echo '<pre class="debug-dd">';
         print_r($content);
         echo '</pre>';
         die();
